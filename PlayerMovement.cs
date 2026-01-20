60ca8668f62f773ce8b1b107770faaae35dc28c4
lukajk1 <73144336+lukajk1@users.noreply.github.com>
2026-01-20 06:45:27 -0800

    add jump

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -5,8 +5,11 @@ public class PlayerMovement : PredictedIdentity<PlayerMovement.MoveInput, Player
 {
     [SerializeField] private float _moveSpeed = 7f;
     [SerializeField] private float _acceleration = 20f;
-    [SerializeField] private float _jumpForce = 7f;
+    [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _planarDamping = 10f;
+    [SerializeField] private float _groundCheckRadius = 0.2f;
+    [SerializeField] private float _jumpCooldown = 0.2f;
+    [SerializeField] private LayerMask _groundMask;
 
     [SerializeField] private FirstPersonCamera _camera;
     [SerializeField] private PredictedRigidbody _rigidbody;
@@ -14,11 +17,16 @@ public class PlayerMovement : PredictedIdentity<PlayerMovement.MoveInput, Player
     {
         if (isOwner) _camera.Init();
     }
+
+
     protected override void Simulate(PlayerMovement.MoveInput input, ref State state, float delta)
     {
         // simulate is the local simulation/interpolation that occurs between packets from the server ig
         // all operations pertaining to state/input should pass through the 'interfaces' referenced by state and input here i.e. don't call input.get... in here
         
+        // delta = time in ms since last tick I guess
+        state.jumpCooldown -= delta;
+
         Vector3 targetVel = (transform.forward * input.moveDirection.y + transform.right * input.moveDirection.x) * _moveSpeed;
         _rigidbody.AddForce(targetVel * _acceleration);
         
@@ -29,7 +37,11 @@ public class PlayerMovement : PredictedIdentity<PlayerMovement.MoveInput, Player
             _rigidbody.velocity = new Vector3(targetVel.x, _rigidbody.velocity.y, targetVel.z);
         }
 
-        if (input.jump) _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        if (input.jump && IsGrounded() && state.jumpCooldown <= 0)
+        {
+            state.jumpCooldown = _jumpCooldown;
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
 
         if (input.cameraForward.HasValue)
         {
@@ -39,6 +51,14 @@ public class PlayerMovement : PredictedIdentity<PlayerMovement.MoveInput, Player
                 _rigidbody.MoveRotation(Quaternion.LookRotation(camForward.normalized));
         }
 
+    }
+
+    private static Collider[] _groundColliders = new Collider[8];
+    private bool IsGrounded()
+    {
+        var hit = Physics.OverlapSphereNonAlloc(transform.position, _groundCheckRadius, _groundColliders, _groundMask);
+        //return hit > 0;
+        return true;
     }
 
     // this will call every frame
@@ -64,8 +84,16 @@ public class PlayerMovement : PredictedIdentity<PlayerMovement.MoveInput, Player
         }
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _groundCheckRadius);  
+    }
+
     public struct State : IPredictedData<State>
     {
+        public float jumpCooldown;
+
         public void Dispose() { }
     }
 
